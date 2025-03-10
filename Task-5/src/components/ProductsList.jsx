b18e07cfd79b21adb3c4be8e292f3ddacf1b83a9
mohamed hassan <141786275+mohamedhassann27@users.noreply.github.com>
2025-03-10 09:27:01 +0200

    Update ProductsList.jsx

--- Task-5/src/components/ProductsList.jsx
+++ Task-5/src/components/ProductsList.jsx
@@ -1,17 +1,30 @@
 import { useEffect , useState } from "react"
 import Product from "./Product";
+import { useActionData } from "react-router";
 
 function ProductsList(){
     let [productsData,setProductsData]=useState([])
+    let [categories,setCataegories]=useState([])
+
+    let api_url="https://fakestoreapi.com/products"
+
+    let fetchData=async (url,set)=>{
+        let response=await fetch(url)
+        let data=await response.json()
+        set(data)
+        console.log(data);
+    }
+
     useEffect(()=>{
-        const fetchData=async ()=>{
-            let response=await fetch("https://fakestoreapi.com/products")
-            let data= await response.json()
-            setProductsData(data)
-            console.log(data);
-            
-        };
-        fetchData()
+        fetchData(api_url,setProductsData)
+        fetchData(`${api_url}/categories`,setCataegories)
+
+        // const fetchData=async ()=>{
+        //     let response=await fetch()
+        //     let data= await response.json()
+        //     setProductsData(data)
+        // };
+        // fetchData()
     },[])
 
     let products=productsData.map((el)=>{
@@ -24,6 +37,19 @@ function ProductsList(){
     return(
         <>
             <h1 className="text-5xl py-10 bg-gradient-to-r from-red-600  to-blue-400 bg-clip-text text-transparent w-contect inline-block">Products</h1>
+
+            <div className="categories mb-10 flex justify-center items-center gap-5">
+                <button className="bg-red-400" onClick={()=>{
+                    fetchData(api_url,setProductsData)
+                }}>All</button>
+                {categories.map((cat)=>{
+                    return <button className="bg-red-400" key={cat} onClick={()=>{
+                        fetchData(`${api_url}/category/${cat}`,setProductsData)
+                    }} > {cat} </button>
+                })}
+
+            </div>
+            
             <div className="productsList grid grid-cols-[repeat(auto-fill,minmax(260px,1fr))] max-w-7xl m-auto gap-x-5 gap-y-20  px-2">
                 {products}
             </div>
